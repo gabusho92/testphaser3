908e385e3ded4b35602ff199d8a8879d3d9fd76e
gabusho92 <61474758+gabusho92@users.noreply.github.com>
2020-03-22 22:55:34 -0300

    Update Scene2.js

--- games/game3/Scene2.js
+++ games/game3/Scene2.js
@@ -2,6 +2,7 @@ class Scene2 extends Phaser.Scene {
 	constructor(){
 		super("GamePlay");
 	}
+
 	create(){
 		this.add.text(20, 20, "Play Game", {font: "25px Arial", fill: "yellow"});
 
@@ -13,14 +14,16 @@ class Scene2 extends Phaser.Scene {
   const wallright = map.createStaticLayer("wallright", tileset, 0, 0);
   const wallleft = map.createStaticLayer("wallleft", tileset, 0, 0);
 
+	var savex = 130;
+	var savey = 110;
 
 	const spawnPlayer = map.findObject("spawn", obj => obj.name === "spawn player");
 	this.player = this.physics.add.sprite(spawnPlayer.x, spawnPlayer.y, "block1").setScale(0.5);
 	this.player.setVelocityX(200);
 	this.player.setGravityY(1400);
 
-	const spawnRock = map.findObject("spawn", obj => obj.name === "spawn rock");
-	this.rock = this.add.image(spawnRock.x, spawnRock.y, "block2").setScale(0.5);
+	const spawnRock = map.findObject("spawn", obj => obj.name === "spawn save");
+	this.save = this.physics.add.sprite(spawnRock.x, spawnRock.y, "block2").setScale(2);
 
 
 		this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
@@ -34,19 +37,30 @@ class Scene2 extends Phaser.Scene {
 		this.physics.add.collider(this.player, enemy, hitBomb, null, this);
 		this.physics.add.collider(this.player, wallright, changeDir, null, this);
 		this.physics.add.collider(this.player, wallleft, changeDirb, null, this);
+		this.physics.add.collider(this.player, this.save, saveGame, null, this);
+
+		function saveGame (player, save){
+        save.disableBody(true, true);
+			savex = 1500;
+			savey = 830;
+			var textsave = this.add.text(1500, 1000, 'SAVE');
+
+		}
 
 		function hitBomb (player, enemy){
-   				this.scene.start("bootGame");
+   				this.player.x = savex;
+   				this.player.y = savey;
+				this.player.setVelocityX(200);
    			}
    		function changeDir (player, wall){
 			this.player.setVelocityX(-200);
-			if (this.player.y > 1000){
+			if (this.player.y > 800 && this.player.x < 1500){
 		this.player.setVelocityY(-450);
 	}
    			}
    		function changeDirb (player, wall){
 			this.player.setVelocityX(200);
-			if (this.player.y > 1000){
+			if (this.player.y > 800 && this.player.x < 1500	){
 		this.player.setVelocityY(-450);
 	}
    			}
@@ -62,16 +76,21 @@ class Scene2 extends Phaser.Scene {
 		this.player.setVelocityY(-450);
 	}
  		}, this);
+
+
+
+ 		    this.input.on('pointermove', function (pointer) {
+
+     
+
+    });
 	}
 
 		moveEnemy(ship, speed){
 		ship.y -=speed;
 	}
 
-	update(){
-		this.moveEnemy(this.rock, 2);
-
-		
+	update(){	
 	
 	if(Phaser.Input.Keyboard.JustDown(this.spacebar)){
 		this.player.setVelocityY(-450);
